6e4461ab333e273d3c32c78b989f096edf464a35
dante <dantehduran@gmail.com>
2022-06-01 20:07:59 -0500

    added menu button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,23 +3,31 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
 import Sidebar from '@/components/sidebar/Sidebar';
+import { BiMenu } from 'react-icons/bi';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
 	return (
 		<div
-			className={`flex h-screen space-x-4 overflow-hidden bg-white p-2 ${
+			className={`relative flex h-screen space-x-4 overflow-hidden  bg-white p-2 ${
 				isSidebarCollapsed ? ' sidebar-collapsed' : ' '
 			}`}
 		>
-			<div className='h sticky w-64 sidebar-collapsed:w-20'>
+			<div className='sticky  hidden w-64 overflow-x-auto sidebar-collapsed:w-20 md:inline-block'>
 				<Sidebar
 					setIsSidebarCollapsed={setIsSidebarCollapsed}
 					isSidebarCollapsed={isSidebarCollapsed}
 				/>
 			</div>
-			<div className='relative flex-1 overflow-x-hidden'>
+
+			<div className='relative flex-1 overflow-auto'>
 				<Component {...pageProps} />
+				<button
+					aria-label='menu'
+					className='fixed top-4 right-6 z-20 block md:hidden'
+				>
+					<BiMenu className='h-8 w-8 text-zinc-800' />
+				</button>
 			</div>
 		</div>
 	);
