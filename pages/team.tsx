b99609577525cb28858f4ccf5884cec28acafa76
dante <dantehduran@gmail.com>
2022-06-12 17:11:43 -0500

    team page

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,9 +1,39 @@
+import { Table } from '@/components/table';
 import type { NextPage } from 'next';
+
+const headers = ['name', 'age', 'status'];
+
+const menberList = [
+	{
+		name: 'John Doe',
+		age: '30',
+		status: 'active',
+	},
+	{
+		name: 'Anna Jane',
+		age: '25',
+		status: 'active',
+	},
+	{
+		name: 'Phin Jones',
+		age: '35',
+		status: 'inactive',
+	},
+];
 
 const Team: NextPage = () => {
 	return (
-		<div>
-			<h2 className='text-xl text-stone-600'>Team</h2>
+		<div className='p-4'>
+			<div className='flex justify-between pb-4'>
+				<h2 className=' text-3xl font-bold text-zinc-700'>Team Members</h2>
+				<button
+					type='button'
+					className='rounded-xl bg-violet-600  px-3 py-2 font-semibold text-zinc-100'
+				>
+					Add Member
+				</button>
+			</div>
+			<Table headers={headers} data={menberList} />
 		</div>
 	);
 };
