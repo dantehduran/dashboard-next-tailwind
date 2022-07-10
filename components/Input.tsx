bbb5d76b22a2b90c40c9a1f82d101fb638fa7f96
dante <dantehduran@gmail.com>
2022-07-10 16:57:46 -0500

    dropdown option on front

--- components/Input.tsx
+++ components/Input.tsx
@@ -44,7 +44,7 @@ const Dropdown = ({ label, options }: { label: string; options: string[] }) => {
 			<Listbox.Label className='absolute left-3 top-1 cursor-text text-xs capitalize text-zinc-600  transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:left-3  peer-placeholder-shown:text-base peer-placeholder-shown:text-zinc-400 peer-focus:top-1 peer-focus:left-3 peer-focus:text-xs'>
 				{label}
 			</Listbox.Label>
-			<Listbox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+			<Listbox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-10'>
 				{options.map(option => (
 					<Listbox.Option
 						key={option}
